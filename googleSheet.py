ccd858a861b6fdf8c16705fa2ac9e25e4e41e8ef
Zaira <52571986+SksZaira@users.noreply.github.com>
2020-08-26 21:51:47 +0530

    Add files via upload

---
+++ googleSheet.py
@@ -0,0 +1,30 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+scope = ['https://www.googleapis.com/auth/drive','https://www.googleapis.com/auth/spreadsheets',
+         'https://www.googleapis.com/auth/drive.file']
+cred = ServiceAccountCredentials.from_json_keyfile_name("creds.json",scope)
+client = gspread.authorize(cred)
+sheet = client.open('Contact Information (Responses)').sheet1
+# data = sheet.get_all_records()
+# print(data)
+data = sheet.get_all_values()
+headers = data.pop(0)
+
+df = pd.DataFrame(data, columns=headers)
+print(df)
+
+# def appendRow(val):
+#     sheet.append_row(val)
+# def if_motion():
+#     state = G.input(2)
+#     if state == 1:
+#         return True
+#     else:
+#         return False
+
+# def send_data_to_cloud(tsk):
+#     aio = Adafruit_IO.Client("ZXX","XXXX")
+#     aio.send('visitors',tsk)
+# a=67
+# send_data_to_cloud(a)
